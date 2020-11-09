9563e1145635482c0279a28f0569873a9678277a
Messier81 <nesanyorku@gmail.com>
2020-11-09 01:01:30 -0500

    added bottom nav, horizontally

--- src/comps/BotNav.jsx
+++ src/comps/BotNav.jsx
@@ -1,15 +1,33 @@
 export default function BotNav() {
   return (
-    <nav class="nav justify-content-center">
-      <a class="nav-link active" href="#">
-        Active
-      </a>
-      <a class="nav-link" href="#">
-        Link
-      </a>
-      <a class="nav-link" href="#">
-        Link
-      </a>
-    </nav>
+    <div className="bot-nav text-center">
+      <div>
+        <a href="new.php">ABOUT</a>
+      </div>
+
+      <div>
+        <a href="view.php">PLB BONUS</a>
+      </div>
+
+      <div>
+        <a href="index.html">CONTACT</a>
+      </div>
+
+      <div>
+        <a href="index.html">SECURITY</a>
+      </div>
+
+      <div>
+        <a href="index.html">RESPONSIBLE PLAY</a>
+      </div>
+
+      <div>
+        <a href="index.html">PRIVACY</a>
+      </div>
+
+      <div>
+        <a href="index.html">TERMS</a>
+      </div>
+    </div>
   );
 }

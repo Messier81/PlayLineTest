7624b9866884a9aee61572917a8f464e493bc358
Messier81 <nesanyorku@gmail.com>
2020-11-09 02:02:40 -0500

    added bottom nav, made it scrollable

--- src/comps/BotNav.jsx
+++ src/comps/BotNav.jsx
@@ -1,32 +1,28 @@
 export default function BotNav() {
   return (
-    <div className="bot-nav text-center">
-      <div>
-        <a href="new.php">ABOUT</a>
-      </div>
-
-      <div>
-        <a href="view.php">PLB BONUS</a>
-      </div>
-
-      <div>
-        <a href="index.html">CONTACT</a>
-      </div>
-
-      <div>
-        <a href="index.html">SECURITY</a>
-      </div>
-
-      <div>
-        <a href="index.html">RESPONSIBLE PLAY</a>
-      </div>
-
-      <div>
-        <a href="index.html">PRIVACY</a>
-      </div>
-
-      <div>
-        <a href="index.html">TERMS</a>
+    <div className="bot-nav">
+      <div className="bot-nav-container">
+        <div className="bot-nav-link-container">
+          <a href="new.php">ABOUT</a>
+        </div>
+        <div className="bot-nav-link-container">
+          <a href="view.php">PLB BONUS</a>
+        </div>
+        <div className="bot-nav-link-container">
+          <a href="index.html">CONTACT</a>
+        </div>
+        <div className="bot-nav-link-container">
+          <a href="index.html">SECURITY</a>
+        </div>
+        <div className="bot-nav-link-container">
+          <a href="index.html">RESPONSIBLE PLAY</a>
+        </div>
+        <div className="bot-nav-link-container">
+          <a href="index.html">PRIVACY</a>
+        </div>
+        <div className="bot-nav-link-container">
+          <a href="index.html">TERMS</a>
+        </div>
       </div>
     </div>
   );
---
+++ src/comps/Players.jsx
@@ -0,0 +1,9 @@
+export default function BotNav() {
+  return (
+    <div className="players-container">
+      <div className="player-images">
+        <div className="Player-image"></div>
+      </div>
+    </div>
+  );
+}
